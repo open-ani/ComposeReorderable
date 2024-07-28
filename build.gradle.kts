5dbdc7a366d51d5e084b845b1afa98cd0aee2533
Him188 <Him188@mamoe.net>
2024-07-28 17:07:29 +0100

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     `maven-publish`
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.multiplatform") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    kotlin("plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "2.0.20-Beta2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20-Beta2" apply false
+    kotlin("plugin.compose") version "2.0.20-Beta2" apply false
     id("org.jetbrains.compose") version "1.6.11" apply false
 }
 
